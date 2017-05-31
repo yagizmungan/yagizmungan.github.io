91bfd1e06c07995c2b65d6a9d162f59644f0a0cf
Yagiz Mungan <yagizmungan@gmail.com>
2017-05-31 09:16:12 -0700

    fancier threes

--- new_design/js/threejs_scene.js
+++ new_design/js/threejs_scene.js
@@ -1,25 +1,46 @@
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+class ThreejsMagic {
+	constructor(){
+		const SCENE = new THREE.Scene();
+		const CAMERA = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-// renderer.setClearColorHex( 0x004500, 1 );
-document.getElementById('threejs-magic').appendChild( renderer.domElement );
+		var renderer = new THREE.WebGLRenderer({antialias: true});
+		renderer.setSize( window.innerWidth, window.innerHeight );
+		// renderer.setClearColorHex( 0x004500, 1 );
+		document.getElementById('threejs-magic').appendChild( renderer.domElement );
 
-var geometry = new THREE.BoxGeometry( 6, 2, 2 );
-var material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+		var geometry = new THREE.TorusKnotGeometry(10, 3, 100, 16);
+		var material = new THREE.MeshPhongMaterial( { 
+      color: 0xaa0000, 
+      // wireframe: true,
+      shading: THREE.FlatShading
+    });
+		var object = new THREE.Mesh(geometry, material);
+		SCENE.add(object);
 
-camera.position.z = 5;
+		CAMERA.position.z = 30;
 
-var render = function () {
-	requestAnimationFrame( render );
+    var lights = [];
+    lights[ 0 ] = new THREE.PointLight( 0xffffff, 1, 0 );
+    lights[ 1 ] = new THREE.PointLight( 0xffffff, 1, 0 );
+    lights[ 2 ] = new THREE.PointLight( 0xffffff, 1, 0 );
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+    lights[ 0 ].position.set( 0, 200, 0 );
+    lights[ 1 ].position.set( 100, 200, 100 );
+    lights[ 2 ].position.set( - 100, - 200, - 100 );
 
-	renderer.render(scene, camera);
-};
+    SCENE.add( lights[ 0 ] );
+    // SCENE.add( lights[ 1 ] );
+    SCENE.add( lights[ 2 ] );
 
-render();
+		var render = function () {
+			requestAnimationFrame( render );
+
+			object.rotation.x += 0.01;
+			object.rotation.y += 0.01;
+
+			renderer.render(SCENE, CAMERA);
+		};
+
+		render();
+	}
+}
